53e55e5e855c2005668ccd74bb8c0e7c6fdeb43e
Baizidbustami <baizidhpi@gmail.com>
2022-04-21 21:55:54 +0600

    first commit

---
+++ src/Home/AddUser/AddUser.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const AddUser = () => {
+    const handleAddUser = event =>{
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+
+        const user = {name, email};
+
+ fetch('http://localhost:5000/user', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(user)
+})
+.then(res => res.json())
+.then(data => {
+  console.log('Success', data);
+
+//   ৬ এর কাজ
+  alert('users added successfully!!!');
+  event.target.reset();
+})
+
+    }
+    return (
+        <div>
+            <h2>Please add a new User</h2>
+            <form onSubmit={handleAddUser}>
+                <input type="text" name="name" placeholder='Name' required />
+                <br />
+                <input type="email" name="email" placeholder='Email' required />
+                <br />
+                <input type="submit" value="Add User" />
+            </form>
+        </div>
+    );
+};
+
+export default AddUser;
